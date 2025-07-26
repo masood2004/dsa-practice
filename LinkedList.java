bf3fede7df5b8c9e8c3e0f90fad3b02c024094cf
Syed Masood Hussain <hmasood3288@gmail.com>
2025-07-26 16:48:14 +0500

    DSA practicing

---
+++ LinkedList.java
@@ -0,0 +1,105 @@
+public class LinkedList {
+
+    Node head, tail = null;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void addMiddle(String data, int position) {
+        Node newNode = new Node(data);
+        if (position == 0) {
+            addFirst(data);
+            return;
+        }
+        Node current = head;
+        int count = 0;
+
+        while (current != null && count < position - 1) {
+            current = current.next;
+            count++;
+        }
+        if (current == null) {
+            System.out.println("Position out of bound.");
+        } else {
+            newNode.next = current.next;
+            current.next = newNode;
+
+            if (newNode.next == null) {
+                tail = newNode;
+            }
+        }
+    }
+
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("List is already empty.");
+        } else {
+            head = head.next;
+
+            if (head == null) {
+                tail = null;
+            }
+        }
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("List is already empty.");
+        } else if (head.next == null) {
+            head = tail = null;
+        } else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.print("End");
+    }
+
+    public static void main(String args[]) {
+        LinkedList l1 = new LinkedList();
+        l1.addFirst("1");
+        l1.addLast("last");
+        l1.addMiddle("Added at 1", 1);
+        l1.deleteFirst();
+        l1.deleteLast();
+        l1.display();
+    }
+}
